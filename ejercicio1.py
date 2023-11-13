16322aed6383af1740afb2156263c13c9fb0adee
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-11-13 20:11:00 +0100

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+
+def saludo(nombre):
+    '''Función que saluda a una persona
+Parámetros: 
+    -nombre: Se establece un nombre para que salude a la persona con ese nombre
+Salida: 
+    Un saludo a una persona'''
+    print('¡Hola', nombre + '!')
+    return 
+saludo('Alex')
