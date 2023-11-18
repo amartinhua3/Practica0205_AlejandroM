bc43ae1d5c7b79177a5b3f4aec06c82fe772622c
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-11-18 12:45:37 +0100

    ejercicio4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,13 @@
-import mean
+
 def nums(n1, n2, n3):
-    return mean([2, 4, 8])
-nums
+    '''Función que calcula la media de tres números
+Parámetros: 
+    n1: Número real
+    n2: Número real
+    n3: Número real
+Salida:
+    Es la media de los tres números establecidos'''
+    return (n1 + n2 + n3) / 3
+nums[2, 4, 8]
+
 
